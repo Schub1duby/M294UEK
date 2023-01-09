cb0d30ccff3ae301285d7f2eb829fcd02b07d665
Larry <skala.larissa@gmail.com>
2023-01-09 14:14:43 +0100

    edited doku and changed structure

---
+++ Larry/.vscode/day1/index.js
@@ -0,0 +1,25 @@
+document.getElementById('title').innerHTML = "hello world";
+
+a = 6;
+b = "6";
+c = a + b;
+console.log(c);
+//66
+
+a = 6;
+b = "6";
+c = a - b;
+console.log(c);
+//0
+
+a = 6;
+b = "6";
+c = a * b;
+console.log(c);
+//36
+
+a = 6;
+b = "6";
+c = a /  b;
+console.log(c);
+//1
